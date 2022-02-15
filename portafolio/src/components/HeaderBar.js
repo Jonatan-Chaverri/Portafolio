c4244e0f4efbee91fb997a326aaa6c640c17df3b
Jonatan Chaverri Castro <jonatan.chaverri@ibm.com>
2022-02-14 21:34:43 -0600

    add header bar collapsed menu

--- portafolio/src/components/HeaderBar.js
+++ portafolio/src/components/HeaderBar.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react'
 
+import { FaBars } from 'react-icons/fa'
+
 
 class HeaderBar extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            showMenu: window.innerWidth < 575,
+            isMenuOpen: false
+        }
+
+        this.displayMenu = this.displayMenu.bind(this)
+        this.openCloseMenu = this.openCloseMenu.bind(this)
+    }
+
+    /*
+     * Event Listener function to display menu in case screen size is resize
+     * to less than 575px
+     */
+    displayMenu(){
+        this.setState({
+            showMenu: window.innerWidth < 570,
+            isMenuOpen: false
+        })
+    }
+
+    /*
+     * Callback function for when user clicks on menu icon
+     */
+    openCloseMenu(){
+        const { isMenuOpen } = this.state
+        this.setState({
+            isMenuOpen: !isMenuOpen
+        })
+    }
+
     componentDidMount() {
+        const { displayMenu } = this
+        window.addEventListener('resize', displayMenu)
         window.onscroll = () => {
             const element = document.getElementById("header-bar")
             let opacity = window.pageYOffset/400
@@ -12,16 +49,28 @@ class HeaderBar extends Component {
     }
 
     render() {
+        const { showMenu } = this.state
+        const { openCloseMenu } = this
         return (
             <div className="header-bar" id="header-bar">
                 <div className="header-bar__title">
                     Jonatan Chaverri
                 </div>
-                <div className="header-bar__menu">
-                    <div className="header-bar__menu-item"> About </div>
-                    <div className="header-bar__menu-item"> Projects </div>
-                    <div className="header-bar__menu-item"> Contact </div>
-                </div>
+                {
+                    showMenu ?
+                    <div className="header-bar__collapsed-menu">
+                        <label
+                          type="button"
+                          onClick={ openCloseMenu }>
+                          <FaBars/>
+                        </label>
+                    </div> :
+                    <div className="header-bar__menu">
+                        <div className="header-bar__menu-item"> About </div>
+                        <div className="header-bar__menu-item"> Projects </div>
+                        <div className="header-bar__menu-item"> Contact </div>
+                    </div>
+                }
             </div>
         )
     }
