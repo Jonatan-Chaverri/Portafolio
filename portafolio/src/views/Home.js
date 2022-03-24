2e74398c79b4781ecdade07ce34c7040f1067ba9
Jonatan Chaverri <jonatan.chaverri@kyndryl.com>
2022-03-23 21:02:05 -0600

    latest commit with tmp changes

--- portafolio/src/views/Home.js
+++ portafolio/src/views/Home.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 import { HeaderBar } from  '../components'
+import { ABOUT_TEXT } from '../strings'
+
+import ABOUT_IMG_1 from '../images/about1.jpeg'
+import ABOUT_IMG_2 from '../images/about2.jpeg'
+import ABOUT_IMG_3 from '../images/about3.jpeg'
 
 class Home extends Component {
 
@@ -29,17 +34,20 @@ class Home extends Component {
                         <div className="home__about-description">
                             <div className="home__about-description-title"> Hello... </div>
                             <div>
-                                Lorem Ipsum is simply dummy text of the printing and
-                                typesetting industry. Lorem Ipsum has been the industry
-                                standard dummy text ever since the 1500s, when an unknown
-                                printer took a galley of type and scrambled it to make
-                                a type specimen book. It has survived not only five
-                                centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised in
-                                the 1960s with the release of Letraset sheets containing
-                                Lorem Ipsum passages, and more recently with desktop
-                                publishing software like Aldus PageMaker including
-                                versions of Lorem Ipsum.
+                                { ABOUT_TEXT }
+                            </div>
+                        </div>
+                        <div className="home__about-images-container">
+                            <div className="home__about-images-wrapper">
+                                <div className="home__about-image-one">
+                                    <img src={ ABOUT_IMG_1 } width="320rem"/>
+                                </div>
+                                <div className="home__about-image-two">
+                                    <img src={ ABOUT_IMG_2 } height="300rem"/>
+                                </div>
+                                <div className="home__about-image-three">
+                                    <img src={ ABOUT_IMG_3 } width="300rem"/>
+                                </div>
                             </div>
                         </div>
                     </div>
